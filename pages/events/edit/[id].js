068c49e70271ec1af8e13c22ca2b48503814fb94
Arik Bidny <arikbidny@gmail.com>
2021-05-01 09:02:39 +0300

    Adding authorization to image upload process

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -176,7 +176,11 @@ const EditEventPage = ({ evt, token }) => {
       </div>
 
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
+        <ImageUpload
+          evtId={evt.id}
+          imageUploaded={imageUploaded}
+          token={token}
+        />
       </Modal>
     </Layout>
   );
